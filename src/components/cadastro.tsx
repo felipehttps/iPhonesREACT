5854e731db64d9cd4e01bc09653d36e00bd45fa9
Pedro Lucas <pp.pedro.sb@gmail.com>
2025-03-25 14:56:02 -0300

    Parte final

--- src/components/cadastro.tsx
+++ src/components/cadastro.tsx
@@ -6,45 +6,65 @@ import { FormEvent, useState } from "react";
 import { supabase } from "../assets/supabase";
 import styles from './footer/home.module.css';
 
-
 export const Cadastro = () => {
-        const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
-        const [message, setMessage] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
-        const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            setMessage("");
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setMessage("");
 
-        const {data, error} = await supabase.auth.signUp({
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; ///
+
+       
+        if (!emailRegex.test(email)) {
+            setMessage("Por favor, insira um e-mail válido!");
+            return; 
+        }
+
+      
+        const { data, error } = await supabase.auth.signUp({
             email: email,
             password: password,
         });
 
-        if (error){
+        if (error) {
             setMessage(error.message);
             return;
         }
-        if (data){
-            setMessage("Logado!");
+        if (data) {
+            setMessage("Cadastro realizado com sucesso!"); 
         }
+
         setEmail("");
         setPassword("");
-
-        };
-
+    };
 
     return (
         <Home>
-            <Logo src="/logotransp.png" ></Logo>
+            <Logo src="/logotransp.png" />
             <Form onSubmit={handleSubmit}>
                 <TitleLogin>CADASTRO</TitleLogin>
                 {message && <span className={styles.tag}>{message}</span>}
-                <InputPro title="Your e-mail" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="e-mail" required></InputPro>
-                <InputPro title="Your password" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="senha" value={password} required></InputPro>
-                <Link to="/" >Ja possui uma conta? Clique aqui</Link>
-                <Button type="submit" >Sign Un</Button>
+                <InputPro 
+                    title="Your e-mail" 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    value={email} 
+                    placeholder="e-mail" 
+                    required 
+                />
+                <InputPro 
+                    title="Your password" 
+                    type="password" 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder="senha" 
+                    value={password} 
+                    required 
+                />
+                <Link to="/">Já possui uma conta? Clique aqui</Link>
+                <Button type="submit">Sign Up</Button>
             </Form>
         </Home>
-    )
-};
+    );
+}; 
